d6050200505963d9eb181cd097d9887dfc1c8449
Natalie EM <espinozanatalie154@gmail.com>
2025-01-03 17:41:31 -0600

    Mi primera version de lista desordemada

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Utilizando git</title>
+</head>
+<body>
+    <ul>
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+    </ul>
+</body>
+</html>
